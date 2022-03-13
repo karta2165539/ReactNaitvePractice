97bc1d4625968c50a3411e97a4bdfc1431bd20fd
MarkHo <karta2165539@gmail.com>
2022-03-13 22:29:17 +0800

    feat: import font file and create default stylesheet

--- GuessNumberApp/screens/StartGameScreen.js
+++ GuessNumberApp/screens/StartGameScreen.js
@@ -4,6 +4,8 @@ import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Ale
 import Card from '../components/Card';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 import Colors from '../constants/colors';
 
 const StartGameScreen = props => {
@@ -42,7 +44,7 @@ const StartGameScreen = props => {
   if (confirmed){
     confirmedOutput = (
       <Card style={styles.chosenNumberCard}>  
-        <Text>You selected</Text>
+        <BodyText>You selected</BodyText>
         <NumberContainer> {selectedNumber} </NumberContainer>
         <Button title='START GAME' onPress={() => props.onStartGame(selectedNumber)}/>
       </Card>
@@ -55,9 +57,9 @@ const StartGameScreen = props => {
       Keyboard.dismiss()
     }}>
     <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game!</Text>
+      <TitleText style={styles.title}>Start a New Game!</TitleText>
       <Card style={styles.inputContainer}>
-        <Text>Select a Number</Text>
+        <BodyText>Select a Number</BodyText>
         <Input 
           style={styles.input} 
           keyboardType='numeric'
@@ -97,7 +99,8 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    marginVertical: 10
+    marginVertical: 10,
+    fontFamily: 'open-sans-bold'
   },
   inputContainer: {
     width: 300,
